019e9c18226c8da00fa633cd0f193dc48897d200
Jordy Coder <59865566+darksos34@users.noreply.github.com>
2021-05-02 20:09:23 +0200

    Update DemoApplication.java

--- src/main/java/com/springDockcontainer/demo/DemoApplication.java
+++ src/main/java/com/springDockcontainer/demo/DemoApplication.java
@@ -1,9 +1,8 @@
 package com.springDockcontainer.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +12,15 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
+		//getMessage will be printed in your command console
+		System.out.println(getMessage());
+
 		return "Hello Docker Builder, Keep on moving forward ";
 	}
-
+	@GetMapping
+	public String getMessage(){
+		return "<h1> Welcome to Docker Tutorial </h1";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
